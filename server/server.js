0dc7ebf5cebc6b3e5fd8edc214e7d899dff16b96
Simon Si <simonsi@outlook.com>
2021-09-01 21:16:52 -0700

    Setup

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+let app = express();
+let port = 3000;
+
+app.use(express.urlencoded());
+app.use(express.static(__dirname + '/../client/dist'));
+
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
