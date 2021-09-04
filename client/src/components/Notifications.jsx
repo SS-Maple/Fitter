47560d7db45a848dffd6bf500647018ddc53a139
tmoes <trmoses42@gmail.com>
2021-09-04 15:25:10 -0500

    Adding some basic file structure

---
+++ client/src/components/Notifications.jsx
@@ -0,0 +1,26 @@
+import React from ('react');
+import styled from ('styled-components');
+import axios from ('axios');
+import PropTypes from ('prop-types');
+
+class Notifications extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  render () {
+    return (
+
+
+
+    )
+  }
+};
+
+Notifications.propTypes = {
+
+};
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const notificationRoutes = require('./notifications.js')
 
 let app = express();
 let port = 3000;
 
 app.use(express.urlencoded());
 app.use(express.static(__dirname + '/../client/dist'));
-
+app.use('/notifications', notificationRoutes);
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
