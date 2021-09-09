578baaee7dc4fdce23d1587f431b9ec103712bb4
Tim Moses <trmoses42@gmail.com>
2021-09-08 22:04:02 -0500

    Built out some routes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,52 @@ const db = require('../database/connect.js');
 app.use(express.urlencoded());
 app.use(express.static(__dirname + '/../client/dist'));
 
+app.get('/notifications', (req, res) => {
+  let userId = 1;
+  db.client.query(`
+    SELECT * FROM notifications
+    WHERE userId = ${userId}
+  `, (err, data) => {
+    if (err) {
+      throw new Error(err);
+      res.send(err);
+    } else {
+      console.log('data from get query\n', data);
+      res.send(data);
+    }
+  })
+});
+
+app.post('/notifications', (req, res) => {
+  let { userId, notificationsText, notificationsTimestamp } = req.query;
+  db.client.query(`
+    INSERT INTO notifications(userId,notificationsText,notificationsTimestamp)
+    VALUES(${userId},${notificationsText},${notificationsTimestamp})
+  `, (err, data) => {
+    if (err) {
+      throw new Error;
+      res.send(err);
+    } else {
+      res.send(data);
+    }
+  })
+
+});
+
+app.put('/notifications', (req, res) => {
+  let userId = 1;
+  db.client.query(`
+    DELETE FROM notifications
+    WHERE userId = $(userId);
+  `, (err, data) => {
+    if (err) {
+      throw new Error(err);
+      res.send(err);
+    } else {
+      res.send(data);
+    }
+  });
+});
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
