40305654e7621606faeca28e129f8f747b8302d1
Tim Moses <trmoses42@gmail.com>
2021-09-08 20:19:01 -0500

    Refactored testing for TopBar

--- _tests_/notificationTests/topbar.test.js
+++ _tests_/notificationTests/topbar.test.js
@@ -1,50 +1,29 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { act } from "react-dom/test-utils";
-import axios from 'axios';
-import { debug } from 'webpack';
-
+import { shallow } from 'enzyme';
 import TopBar from '../../client/src/components/SharedComponents/TopBar.jsx';
+import Logo from '../../client/src/components/SharedComponents/TopBarComponents/Logo.jsx';
+import NotificationIcon from '../../client/src/components/SharedComponents/TopBarComponents/NotificationIcon.jsx';
+
+const $ = require('jquery');
 
 require('babel-polyfill');
 
 describe('Renders the TopBar component', () => {
-  render(<TopBar />);
+  test('renders topBar', () => {
+    render(<TopBar />);
+  });
 });
 
-describe('Renders the topbar component\'s 4 main elements', () => {
-  test('renders emptyDiv'), () => {
-    let emptyDiv = document.getElementById('emptyDiv');
-    expect(emptyDiv).toBeTruthy();
-  };
-
-  test('renders logoButton', () => {
-    expect(screen.getByText(/FITTER/)).toBeInTheDocument();
+describe('Renders emptyDiv', () => {
+  test('renders emptyDiv', () => {
+    render(<Logo />);
   });
+});
 
+describe('Renders navigationButton', () => {
   test('renders navigationButton', () => {
-    let navigationButton = document.getElementById('navigationButton');
-    expect(navigationButton).toBeTruthy();
-  });
-
-  test('renders navigationLogo', () => {
-    expect(screen.getByText(/&#9742/)).toBeInTheDocument();
-  });
-});
-
-describe('logoButton event handler', () => {
-  test('button fires when clicked', () => {
-    let logoButton = document.getElementById('logoButton');
-    fireEvent.click(logoButton);
-    expect(onClick).toBeCalled();
-  });
-});
-
-describe('notificationButton cevent handler', () => {
-  test('button fires when clicked', () => {
-    let notificationButton = document.getElementById('notificationButton');
-    fireEvent.click(notificationButton);
-    expect(onClick).toBeCalled();
+    render(<NotificationIcon />);
   });
 });
