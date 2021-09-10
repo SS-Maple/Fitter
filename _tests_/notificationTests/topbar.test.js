0e5bc7f079b8d84f8b91e1d3ff68f5e1d1a68cab
Tim Moses <trmoses42@gmail.com>
2021-09-10 00:32:28 -0500

    Fixed topbar test suite

--- _tests_/notificationTests/topbar.test.js
+++ _tests_/notificationTests/topbar.test.js
@@ -6,8 +6,6 @@ import TopBar from '../../client/src/components/SharedComponents/TopBar.jsx';
 import Logo from '../../client/src/components/SharedComponents/TopBarComponents/Logo.jsx';
 import NotificationIcon from '../../client/src/components/SharedComponents/TopBarComponents/NotificationIcon.jsx';
 
-const $ = require('jquery');
-
 require('babel-polyfill');
 
 describe('Renders the TopBar component', () => {
@@ -27,19 +25,3 @@ describe('Renders navigationButton', () => {
     render(<NotificationIcon />);
   });
 });
-
-describe('logoButton event handler fires when clicked', () => {
-  fireEvent(node: HTMLElement, event: Event);
-  fireEvent(
-    getByText(container, 'FITTER'),
-    new MouseEvent('click', {
-
-    })
-  )
-});
-
-// describe('notificationButton event handler fires when clicked', () => {
-//   fireEvent(
-//     getByText(container, 'Fitter')
-//   )
-// });
