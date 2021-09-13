7ae8109c679bf00e1d6ca2eb03f7fa4948079a00
Simon Si <simonsi@outlook.com>
2021-09-13 09:07:17 -0700

    Added search for food in calorie modal

--- client/src/components/myProfile/addCalorieModal.jsx
+++ client/src/components/myProfile/addCalorieModal.jsx
@@ -6,6 +6,8 @@ class AddCalorieModal extends React.Component{
     super(props);
     this.state = {
       value: '',
+      selectedFood: '',
+      choices: [],
       calorie: 0
     }
   }
@@ -17,21 +19,44 @@ class AddCalorieModal extends React.Component{
     })
   }
 
-  handleChange(e){
+  handleChange(e) {
     this.setState({
       value: e.target.value
     })
 
-    if(e.target.value.length > 3){
-      return axios('https://api.edamam.com/api/food-database/v2/parser', {
+    if(e.target.value.length >= 3){
+      return axios('https://api.edamam.com/auto-complete', {
       params:{
           app_id: 'a200e091',
           app_key: '925b4ad43263447efc8f2be33b2bc28a',
-          ingr: e.target.value
+          q: e.target.value
+        }
+      })
+    .then(results => {
+      this.setState({
+        choices: results.data
+      })
+    })
+    }
+  }
+
+  handleSelect(e){
+    this.setState({
+      choices: [],
+      selectedFood: e.target.innerText
+    })
+
+
+    return axios('https://api.edamam.com/api/food-database/v2/parser', {
+      params:{
+          app_id: 'a200e091',
+          app_key: '925b4ad43263447efc8f2be33b2bc28a',
+          ingr: e.target.innerText
       }
     })
     .then(result => this.setState({calorie:result.data.parsed[0].food.nutrients.ENERC_KCAL}))
-    }
+
+
   }
 
   render(){
@@ -53,10 +78,13 @@ class AddCalorieModal extends React.Component{
               this.props.close('calorieShow')}
               }>
               <input className='food-input' type='text' placeholder='Enter Meal' value={this.state.value} onChange={(e) => this.handleChange(e)}></input>
+              <div className='food-options' >
+                {this.state.choices.map((food, i) => <div className='food-select' key={i} onClick={(e) => this.handleSelect(e)}> {food} </div>)}
+              </div>
               <input type='submit' value='Submit'></input>
             </form>
             <div className='calorie-display'>
-              <div className='food-calorie-display d1'>{this.state.value}</div>
+              <div className='food-calorie-display d1'>{this.state.selectedFood}</div>
               <div className='food-calorie-display d2'>{this.state.calorie} cal</div>
             </div>
           </div>
