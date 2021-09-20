c4d3b0076cb38349161e074df17817b64ab4ea2f
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-09-19 22:15:42 -0700

    EOD

---
+++ client/src/components/HomeFeed/scripts/sortFriends.js
@@ -0,0 +1,17 @@
+const sortFriends = function (friends, userId) {
+  friends.forEach((friend, index) => {
+    // returns negative if they missed the goal
+    let water = Math.abs(100 - (friend['userdata'][index]['wateraverage'] * 100))
+    // returns negative if goal is exceeded
+    let calories = Math.abs(100 - (friend['userdata'][index]['caloriesaverage'] * 100))
+    let calculate = water + calories;
+    friend['sorting'] = calculate.toFixed(2);
+    friend['wateraverage'] = friend['userdata'][index]['wateraverage'];
+    friend['caloriesaverage'] = friend['userdata'][index]['caloriesaverage'];
+  })
+  friends.sort((a, b) => a.sorting - b.sorting)
+    .forEach((user, index) => user['ranks'] = (index + 1))
+}
+
+
+export default sortFriends;
