22dff171adbab4df1603658e484ecb2a1e7496de
Griselda Sanchez <66498470+griseldasanchez@users.noreply.github.com>
2021-09-20 20:08:06 -0700

    Delete firebase.config.js

--- firebase.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyBPibG1sQV_lXCwxJdnEUV11jJ-WTi0llE",
-  authDomain: "fitter-6acaa.firebaseapp.com",
-  projectId: "fitter-6acaa",
-  storageBucket: "fitter-6acaa.appspot.com",
-  messagingSenderId: "644346611079",
-  appId: "1:644346611079:web:f66bb6d1a692be3e21ff22",
-  measurementId: "G-HPYWC8CMJP"
-};
