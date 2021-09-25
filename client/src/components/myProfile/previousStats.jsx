f59db3e70fddd921cd508ee07f73b208c240e0ac
griseldasanchez <gs.griseldasanchez@gmail.com>
2021-09-25 00:41:56 -0700

    Stats redering

--- client/src/components/myProfile/previousStats.jsx
+++ client/src/components/myProfile/previousStats.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PreviousStats = ({ stats, goals, handleShare }) => {
-  if (!stats) {
+  if (stats !== null) {
     return stats.map((stat, i) => {
       let waterGoal = ((stat.water / goals.watergoal) * 100).toFixed(0);
       let calorieGoal = ((stat.calories / goals.caloriegoal) * 100).toFixed(0);
